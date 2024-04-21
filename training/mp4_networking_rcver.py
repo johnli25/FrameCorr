a3e2beb2bf5f71912bac2a78966193c779fc1306
John Li <warriorjohn36@gmail.com>
2024-04-21 03:33:01 -0500

    fixed deepaks bugs

--- training/mp4_networking_rcver.py
+++ training/mp4_networking_rcver.py
@@ -14,7 +14,7 @@ def recvall(sock, count):
         count -= len(newbuf)
     return buf
 
-host = "172.22.154.247"
+host = "172.22.153.20"
 port = 50013
 MAX_PAYLOAD = 500
 DELIMITER = b'\xFF\x00\xFF' 
@@ -30,20 +30,23 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s_sock:
     s_sock.bind((host, port))
     s_sock.listen()
     sock_conn, sock_addr = s_sock.accept()
-    video_folder = "/home/deepakn3/Progressive-Neural-Compression/compressed_videos_output"
+    video_folder = "/home/johnwl2/FrameCorr/Progressive-Neural-Compression/compressed_videos_output"
         
     # Receive video data in chunks and write to file
-    file_iter = iter(os.listdir(video_folder))
+    # file_iter = iter(os.listdir(video_folder))
+    file_iter = iter(sorted(os.listdir(video_folder)))
     
     # length_bytes = sock_conn.recv(4)
     # video_length = struct.unpack('!I', length_bytes)[0]
     #sock_conn.send(b'received') 
-    output_folder = "/home/deepakn3/Progressive-Neural-Compression/received_mp4s_test"
-    print(next(file_iter))
+    output_folder = "/home/johnwl2/FrameCorr/Progressive-Neural-Compression/received_mp4s_test_john"
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
     output_filename = next(file_iter)
     output_path = os.path.join(output_folder, output_filename)
     buffer = b''
     start = time.perf_counter()
+    total_bytes_received = 0
     while True:
         with open(output_path, 'ab+') as f:
             print(output_path)
@@ -54,16 +57,20 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s_sock:
                     break 
                 buffer += chunk
                 received_bytes += len(chunk)
-                print(received_bytes)
+            print(received_bytes)
             mp4_bytes, _, buffer = buffer.partition(DELIMITER)
             f.write(mp4_bytes)
             output_filename = next_filename(file_iter)
+
             if output_filename == None:
                 break    
             output_path = os.path.join(output_folder, output_filename)
+        total_bytes_received += received_bytes
+
     sock_conn.close()
     end = time.perf_counter()
     duration = end - start
+    print(f"Received {total_bytes_received} bytes")
     print(f"Code execution time: {duration:.6f} seconds") 
 #metrics_input_folder = "/home/deepakn3/Progressive-Neural-Compression/send_vids"
 #create_decoded_output_frames(metrics_input_folder, output_frames_directory):
